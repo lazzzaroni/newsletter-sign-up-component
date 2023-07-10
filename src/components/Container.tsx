81e702a3418a4605729a94a31e086421d86c7fdd
lazzzaroni <iamnotlazzaroni@gmail.com>
2023-07-10 19:08:30 +0200

    done for today

--- src/components/Container.tsx
+++ src/components/Container.tsx
@@ -1,20 +1,40 @@
-
 import { cn } from "@/lib/utils";
-
 
 export default function Container() {
   return (
     <section
       className={cn(
         "flex h-[842px] w-[375px] flex-col bg-neutral-white",
-        "lg:h-[641px] lg:w-[928px] lg:rounded-3xl lg:flex-row"
+        "lg:h-[641px] lg:w-[928px] lg:flex-row-reverse lg:rounded-3xl"
       )}
     >
-      <div className="w-full lg:p-16">
-        <div className=" bg-neutral-grey lg:h-full"></div>
+      <div className="lg:p-6">
+        <div className="h-full w-auto rounded-xl lg:w-[400px]">
+          <picture>
+            <source
+              media="(min-width:1024px)"
+              srcSet="/illustration-sign-up-desktop.svg"
+            />
+            <img
+              src="/illustration-sign-up-mobile.svg"
+              alt="Illustration"
+              className="h-auto w-full"
+            />
+          </picture>
+        </div>
       </div>
-      <div className="p-6">
-        <div className="w-[400px] h-full bg-primary-tomato rounded-xl"></div>
+      <div className="w-full lg:p-16">
+        <h1>Stay updated!</h1>{" "}
+        <p>Join 60,000+ product managers receiving monthly updates on:</p>
+        <ul>
+          <li>Product discovery and building what matters </li>
+          <li>Measuring to ensure updates are a success</li>
+          <li>And much more!</li>
+        </ul>
+        <label htmlFor="">
+          Email address <input type="text" placeholder="email@company.com" />
+        </label>
+        <button>Subscribe to monthly newsletter</button>
       </div>
     </section>
   );
