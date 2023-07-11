96f167a421b982589056d1b3e4a13e34813a8ba3
lazzzaroni <iamnotlazzaroni@gmail.com>
2023-07-11 16:50:40 +0200

    update README + prepare for deploy

--- next.config.mjs
+++ next.config.mjs
@@ -6,7 +6,7 @@ await import("./src/env.mjs");
 
 /** @type {import("next").NextConfig} */
 const config = {
-    // basePath: "/newsletter-sign-up-component",
+  basePath: "/newsletter-sign-up-component",
   reactStrictMode: true,
 
   /**
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -14,27 +14,24 @@ export default function Home() {
         <link
           rel="shortcut icon"
           type="image/x-icon"
-          href="/images/favicon.ico"
-          // href="/newsletter-sign-up-component/images/favicon.ico"
+          href="/newsletter-sign-up-component/images/favicon.ico"
         />
         <link
           rel="apple-touch-icon"
           sizes="180x180"
-          href="/images/apple-touch-icon.png"
-          // href="/newsletter-sign-up-component/images/apple-touch-icon.png"
+          href="/newsletter-sign-up-component/images/apple-touch-icon.png"
         />
         <link
           rel="icon"
           type="image/png"
           sizes="32x32"
-          href="/images/favicon-32x32.png"
-          // href="/newsletter-sign-up-component/images/favicon-32x32.png"
+          href="/newsletter-sign-up-component/images/favicon-32x32.png"
         />
         <link
           rel="icon"
           type="image/png"
           sizes="16x16"
-          href="/interactive-rating-component/images/favicon-16x16.png"
+          href="/newsletter-sign-up-component/images/favicon-16x16.png"
         />
       </Head>
       <Container />
